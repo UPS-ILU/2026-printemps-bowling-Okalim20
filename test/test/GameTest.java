ab30571bc157c75339c5c4e80ddd1e33095b2548
Okalim20 <liam.duneau-delis@laposte.net>
2026-04-16 17:15:21 +0200

    Niveau 2

--- test/test/GameTest.java
+++ test/test/GameTest.java
@@ -2,16 +2,33 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Iterator;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import tdd.Game;
 
 class GameTest {
 
+	private Game game;
+	
+	@BeforeEach
+	void init() {
+		game = new Game();
+	}
+	
 	@Test
 	void testEntreeVide() {
-		Game game = new Game();
-		game.score();
+		assertEquals(0,game.score());
 	}
-
+	
+	@Test
+	void testVingtRoll() {
+		for(int i=0;i<20;i++) {
+			game.roll(0);
+		}
+		
+		assertEquals(0, game.score());
+	}
 }
